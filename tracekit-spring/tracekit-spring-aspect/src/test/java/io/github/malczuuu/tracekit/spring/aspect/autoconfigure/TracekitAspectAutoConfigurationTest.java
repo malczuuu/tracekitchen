3430ca080a3779d2c62ac0c393fa586b97a415b8
Damian Malczewski <damian.m.malczewski@gmail.com>
2026-02-10 19:47:04 +0100

    Add tests for enabled flags

--- tracekit-spring/tracekit-spring-aspect/src/test/java/io/github/malczuuu/tracekit/spring/aspect/autoconfigure/TracekitAspectAutoConfigurationTest.java
+++ tracekit-spring/tracekit-spring-aspect/src/test/java/io/github/malczuuu/tracekit/spring/aspect/autoconfigure/TracekitAspectAutoConfigurationTest.java
@@ -1,14 +1,87 @@
 package io.github.malczuuu.tracekit.spring.aspect.autoconfigure;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.malczuuu.tracekit.spring.aspect.ScheduledTracingAspect;
+import io.github.malczuuu.tracekit.spring.aspect.TraceableTracingAspect;
 import io.github.malczuuu.tracekit.spring.autoconfigure.TracekitAutoConfiguration;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {TracekitAutoConfiguration.class, TracekitAspectAutoConfiguration.class})
-@ActiveProfiles(profiles = {"test"})
 class TracekitAspectAutoConfigurationTest {
 
-  @Test
-  void contextLoads() {}
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class, TracekitAspectAutoConfiguration.class},
+      properties = {"tracekit.aspect.enabled=true"})
+  @Nested
+  class WithTracekitAspectEnabledTrue {
+
+    @Autowired(required = false)
+    ScheduledTracingAspect scheduledTracingAspect;
+
+    @Autowired(required = false)
+    TraceableTracingAspect traceableTracingAspect;
+
+    @Test
+    void contextLoads() {
+      assertThat(scheduledTracingAspect).isNotNull();
+      assertThat(traceableTracingAspect).isNotNull();
+    }
+  }
+
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class, TracekitAspectAutoConfiguration.class},
+      properties = {"tracekit.aspect.enabled=false"})
+  @Nested
+  class WithTracekitAspectEnabledFalse {
+
+    @Autowired(required = false)
+    ScheduledTracingAspect scheduledTracingAspect;
+
+    @Autowired(required = false)
+    TraceableTracingAspect traceableTracingAspect;
+
+    @Test
+    void contextLoads() {
+      assertThat(scheduledTracingAspect).isNull();
+      assertThat(traceableTracingAspect).isNull();
+    }
+  }
+
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class, TracekitAspectAutoConfiguration.class},
+      properties = {"tracekit.aspect.enabled=true", "tracekit.aspect.scheduled-enabled=false"})
+  @Nested
+  class WithScheduledAspectEnabledFalse {
+
+    @Autowired(required = false)
+    ScheduledTracingAspect scheduledTracingAspect;
+
+    @Test
+    void contextLoads() {
+      assertThat(scheduledTracingAspect).isNull();
+    }
+  }
+
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class, TracekitAspectAutoConfiguration.class},
+      properties = {"tracekit.aspect.enabled=true", "tracekit.aspect.traceable-enabled=false"})
+  @Nested
+  class WithTraceableAspectEnabledFalse {
+
+    @Autowired(required = false)
+    TraceableTracingAspect traceableTracingAspect;
+
+    @Test
+    void contextLoads() {
+      assertThat(traceableTracingAspect).isNull();
+    }
+  }
 }
--- tracekit-spring/tracekit-spring-core/src/test/java/io/github/malczuuu/tracekit/spring/autoconfigure/TracekitAutoConfigurationTest.java
+++ tracekit-spring/tracekit-spring-core/src/test/java/io/github/malczuuu/tracekit/spring/autoconfigure/TracekitAutoConfigurationTest.java
@@ -1,13 +1,72 @@
 package io.github.malczuuu.tracekit.spring.autoconfigure;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import io.github.malczuuu.tracekit.Tracer;
+import io.github.malczuuu.tracekit.simple.SimpleTracerBuilder;
+import io.github.malczuuu.tracekit.spring.LoggingContextSimpleTracerBuilderCustomizer;
+import io.github.malczuuu.tracekit.spring.TracingTaskDecorator;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest(classes = {TracekitAutoConfiguration.class})
-@ActiveProfiles(profiles = {"test"})
 class TracekitAutoConfigurationTest {
 
-  @Test
-  void contextLoads() {}
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class},
+      properties = {"tracekit.enabled=true"})
+  @Nested
+  class WithEnabledTrue {
+
+    @Autowired(required = false)
+    private LoggingContextSimpleTracerBuilderCustomizer loggingContextSimpleTracerBuilderCustomizer;
+
+    @Autowired(required = false)
+    private SimpleTracerBuilder simpleTracerBuilder;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private TracingTaskDecorator tracingTaskDecorator;
+
+    @Test
+    void contextLoads() {
+      assertThat(loggingContextSimpleTracerBuilderCustomizer).isNotNull();
+      assertThat(simpleTracerBuilder).isNotNull();
+      assertThat(tracer).isNotNull();
+      assertThat(tracingTaskDecorator).isNotNull();
+    }
+  }
+
+  @ActiveProfiles(profiles = {"test"})
+  @SpringBootTest(
+      classes = {TracekitAutoConfiguration.class},
+      properties = {"tracekit.enabled=false"})
+  @Nested
+  class WithEnabledFalse {
+
+    @Autowired(required = false)
+    private LoggingContextSimpleTracerBuilderCustomizer loggingContextSimpleTracerBuilderCustomizer;
+
+    @Autowired(required = false)
+    private SimpleTracerBuilder simpleTracerBuilder;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private TracingTaskDecorator tracingTaskDecorator;
+
+    @Test
+    void contextLoads() {
+      assertThat(loggingContextSimpleTracerBuilderCustomizer).isNull();
+      assertThat(simpleTracerBuilder).isNull();
+      assertThat(tracer).isNull();
+      assertThat(tracingTaskDecorator).isNull();
+    }
+  }
 }
